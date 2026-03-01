a2cc31fe429a52c0690ac814fb9e8d934d36b614
brandaoplaster <brandaoplaster@gmail.com>
2026-03-01 17:18:51 -0300

    feat: Add baseButton component

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  type?: 'button' | 'submit' | 'reset'
+  label: string
+}>()
+</script>
+
+<template>
+  <button
+    :type="type ?? 'button'"
+    class="bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700 transition-colors"
+  >
+    {{ label }}
+  </button>
+</template>
