3a5a74c044378928e1022251099e5a7ffcdccead
brandaoplaster <brandaoplaster@gmail.com>
2026-03-01 14:49:04 -0300

    chore: Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
+  <div class="min-h-screen bg-gray-50">
+    <header class="bg-white shadow-sm">
+      <nav class="container mx-auto px-4 py-4">
+        <RouterLink to="/" class="text-xl font-bold text-gray-900">
+          NoteKeeper
+        </RouterLink>
       </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </header>
+    
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
